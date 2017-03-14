f46087f0a530b4c33909d64ecff6f049debc41d2
xtzeng <xiaoti_zeng@126.com>
2017-03-14 18:36:22 +0800

    add xmq-spring-batch-jdbc-paging

---
+++ xmq-spring-batch-jdbc-paging/src/main/java/com/xmq/ContactRowMapper.java
@@ -0,0 +1,26 @@
+package com.xmq;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class ContactRowMapper implements RowMapper<Contact> {
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.jdbc.core.RowMapper#mapRow(java.sql.ResultSet, int)
+	 */
+	@Override
+	public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO 02 take a look at the ContactRowMapper
+		// it implements the logic to convert the JDBC ResultSet into a domain object
+		// the item reader will use it
+		return new Contact(
+				rs.getLong("id"),
+				rs.getString("firstname"),
+				rs.getString("lastname"), 
+				rs.getDate("birth")
+		);
+	}
+}
