7c1cac21a16fac8a2161bbf7e31760ce6b45cce0
xtzeng <xiaoti_zeng@126.com>
2017-03-14 14:55:43 +0800

    add xmq-spring-batch-flat-file-reading

---
+++ xmq-spring-batch-flat-file-reading/src/main/java/com/xmq/ContactFieldSetMapper.java
@@ -0,0 +1,28 @@
+package com.xmq;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+/**
+ * 
+ */
+public class ContactFieldSetMapper implements FieldSetMapper<Contact> {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.batch.item.file.mapping.FieldSetMapper#mapFieldSet
+	 * (org.springframework.batch.item.file.transform.FieldSet)
+	 */
+	@Override
+	public Contact mapFieldSet(FieldSet fieldSet) throws BindException {
+		// TODO 02 create a Contact from the FieldSet
+		return new Contact(
+				fieldSet.readString("firstname"),
+				fieldSet.readString("lastname"), 
+				fieldSet.readDate("birth","yyyy-MM-dd")
+		);
+	}
+}
---
+++ xmq-spring-batch-flat-file-reading/src/main/java/com/xmq/JdbcContactItemWriter.java
@@ -0,0 +1,36 @@
+package com.xmq;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class JdbcContactItemWriter implements ItemWriter<Contact> {
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	public JdbcContactItemWriter(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.batch.item.ItemWriter#write(java.util.List)
+	 */
+	@Override
+	public void write(List<? extends Contact> items) throws Exception {
+		for(Contact item : items) {
+//			Long id = jdbcTemplate.queryForObject("select contact_seq.nextval from dual",Long.class).longValue();
+			jdbcTemplate.update(
+				"insert into contact (firstname,lastname,birth) values (?,?,?)",
+				item.getFirstname(),item.getLastname(),item.getBirth()
+			);
+		}
+	}
+
+}
